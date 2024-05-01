8cce5b29273756971b91c5dbf682f8ccb0e50de0
jose <jgabriel2608@gmail>
2024-05-01 16:32:49 -0500

    trabajo en clase

---
+++ ejemplos-clases-constructores-diagramas/ejemplo0/codigo/ManejoClases/src/paquete3/Pricipal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete3;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Pricipal {
+      public static void main(String[] args) {
+          /*
+          En esta clases de metodo main se ha creado un connstuctor por defecto ya
+          que en la clase computador no hay ningun contructor tonses eso hace que por defecto
+          utilize el contructo que se crea al rato de crear un objeto
+          */
+               
+          Computadora computadoraTrabajo = new Computadora();
+      }
+}
